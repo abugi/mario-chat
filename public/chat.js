f7384ba9c48862480271ed26f88b361a4483d82b
abugi <abugiboi@gmail.com>
2017-12-19 21:10:04 +0100

    initial commit

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,8 @@
+//connect socket
 const socket = io.connect('localhost:3000');
+
+//DOM Query
+const output = document.querySelector('#output');
+const message = document.querySelector('#message');
+const handle = document.querySelector('#handle');
+const send = document.querySelector('#send');
