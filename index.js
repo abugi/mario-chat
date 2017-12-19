62daf1940b38ee1cd10033d08d060bda2eba092b
abugi <abugiboi@gmail.com>
2017-12-19 16:55:50 +0100

    initial commit

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var express = require('express');
 var app = express();
 
 var server = app.listen(3000, function(){
-  console.log('chat app live on port 3K');
+  console.log('chat app live on port 3k');
 });
