fe5b9d027efec3227a365f50caec02a977c9cc8b
abugi <abugiboi@gmail.com>
2017-12-19 17:19:58 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
 
 //server
 var server = app.listen(3000, function(){
@@ -10,4 +11,4 @@ var server = app.listen(3000, function(){
 app.use(express.static('public'));
 
 //setting up sokcet.io
-
+io.socket(server);
