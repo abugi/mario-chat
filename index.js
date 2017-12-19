a027381083e28159615f1811f9fcadbc43c8a48a
abugi <abugiboi@gmail.com>
2017-12-19 17:17:03 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express();
 
+//server
 var server = app.listen(3000, function(){
   console.log('chat app live on port 3k');
 });
 
+//serving static files
 app.use(express.static('public'));
+
+//setting up sokcet.io
+
